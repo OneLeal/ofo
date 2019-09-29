8dbdf10b094b881011e440beaf76bc8b3829944c
weizhenke <15639067881@163.com>
2019-09-29 19:12:59 +0800

    fix: 增加一个简单的事件

--- ofo-system/src/App.js
+++ ofo-system/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from 'antd'
+import { Button, message } from 'antd'
 import './App.css'
 
 function App () {
@@ -7,12 +7,16 @@ function App () {
   return (
     <div className="App">
       <div>
-        <Button style={ marginLeft } type={'primary'}>按 钮</Button>
-        <Button style={ marginLeft } type={'danger'}>按 钮</Button>
-        <Button style={ marginLeft }>按 钮</Button>
+        <Button onClick={() => infoMsg('主按钮')} style={ marginLeft } type={'primary'}>按 钮</Button>
+        <Button onClick={() => infoMsg('危险按钮')} style={ marginLeft } type={'danger'}>按 钮</Button>
+        <Button onClick={() => infoMsg('默认按钮')} style={ marginLeft }>按 钮</Button>
       </div>
     </div>
   )
+}
+
+function infoMsg (str) {
+  message.info(str)
 }
 
 export default App
